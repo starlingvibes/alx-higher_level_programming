226eb031b64180dd86878262d45a8dd74681f812
starlingvibes <starlingroot@protonmail.com>
2022-06-28 12:05:45 +0000

    print combination

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,8 +1,4 @@
 #!/usr/bin/python3
-for i in range(0, 100):
-    if i < 10:
-        i = '0' + str(i)
-    if i == 99:
-        print(i)
-        break
-    print(i, end=', ')
+for i in range(0, 99):
+    print("{:02d}".format(i), end=', ')
+print("{:02d}".format(i + 1))
