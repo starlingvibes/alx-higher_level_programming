390baf7a7e03c952d128074f8d26cd1f0cf315cc
Chidera Anichebe <starlingroot@protonmail.com>
2022-06-28 15:22:51 +0100

    fixed indentation

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def islower(c):
      if ord(c) >= ord('a') and ord(c) <= ord('z'):
-        return True
-    else:
-        return False
+          return True
+     else:
+          return False
