b1e2d766ac4ccc227a244e01f89ef07789f90d3b
starlingvibes <starlingroot@protonmail.com>
2022-06-28 12:09:57 +0000

    is lower?

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,6 +1,2 @@
 def islower(c):
-    asciicode = ord(c)
-    if asciicode >= 97 and asciicode <= 122:
-        return True
-    elif asciicode >= 65 and asciicode <= 90:
-        return False
+    return(ord('a') <= ord(c) <= ord('z'))
