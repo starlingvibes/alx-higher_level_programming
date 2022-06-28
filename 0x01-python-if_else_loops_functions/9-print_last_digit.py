7fcf352b241546a2e42c663b4baa72b5ce1d5357
starlingvibes <starlingroot@protonmail.com>
2022-06-28 12:19:36 +0000

    fixed bug

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-    if number > 0:
+    if number < 0:
         print("{:d}".format(-(number % -10)), end='')
         return(-(number % -10))
     else:
