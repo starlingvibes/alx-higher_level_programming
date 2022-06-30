eda365dd884d66f49918d786ecfa23ae929ff66b
starlingvibes <starlingroot@protonmail.com>
2022-06-30 05:46:16 -0700

    basic calculator

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,24 +1,18 @@
 #!/usr/bin/python3
-import sys
-import calculator_1
+if __name__ == "__main__":
+    from sys import argv
+    from calculator_1 import add, sub, mul, div
 
+    operators = {"+": add, "-": sub, "*": mul, "/": div}
 
-if len(sys.argv) != 4:
-    print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-    exit(1)
-else:
-    operators = ['+', '-', '*', '/']
-    if sys.argv[2] not in operators:
+    if len(argv) != 4:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
+    elif operators.get(argv[2]) is not None:
+        a = int(argv[1])
+        b = int(argv[3])
+        func = operators[argv[2]](a, b) # brilliant lol
+        print("{} {} {} = {}".format(argv[1], argv[2], argv[3], func))
+    else:
         print("Unknown operator. Available operators: +, -, * and /")
         exit(1)
-    else:
-        a = int(sys.argv[1])
-        b = int(sys.argv[3])
-        if sys.argv[2] == "+":
-            print(f"{a} + {b} = {calculator_1.add(a, b)}")
-        elif sys.argv[2] == "-":
-             print(f"{a} - {b} = {calculator_1.sub(a, b)}")
-        elif sys.argv[2] == "*":
-            print(f"{a} * {b} = {calculator_1.mul(a, b)}")
-        else:
-            print(f"{a} / {b} = {calculator_1.div(a, b)}")
