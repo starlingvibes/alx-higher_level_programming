03ed91ef580ff22d7d28901687d2e4cf7d0e3ec0
starlingvibes <starlingroot@protonmail.com>
2022-06-30 05:28:59 -0700

    infinite add

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
-import sys
+if __name__ == "__main__":
+    from sys import argv
 
+    num = len(argv)
+    result = 0
 
-sumint = 0
-for i in range(1, len(sys.argv)):
-    sumint += int(sys.argv[i])
-print(f"{sumint}")
+    if num > 1:
+        for i in range(1, num):
+            result += int(argv[i]
+    print("{:d}".format(result))
