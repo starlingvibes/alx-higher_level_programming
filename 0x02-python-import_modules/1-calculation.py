b0859a25c8ee90588e1aeaa30ae9e9971373889c
starlingvibes <starlingroot@protonmail.com>
2022-06-30 05:21:37 -0700

    calculation from module

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-from calculator_1 import add, sub, mul div
-
-
-print(f"{add(10, 5)}")
-print(f"{sub(10, 5)}")
-print(f"{mul(10, 5)}")
-print(f"{div(10, 5)}")
+if __name__ == "__main__":
+    from calculator_1 import add, sub, mul div
+    a = 10
+    b = 5
+    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+    print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
+    print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
+    print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
