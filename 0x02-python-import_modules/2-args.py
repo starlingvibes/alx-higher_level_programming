6f04494635ecda858c707d1bb9dab1f1edd757a4
starlingvibes <starlingroot@protonmail.com>
2022-06-30 05:26:36 -0700

    working with arguments

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python3
-import sys
+if __name__ == "__main__":
+    from sys import argv
 
 
-arglength = len(sys.argv) - 1
-if arglength <= 0:
-    print(f"{arglength} arguments.")
-elif arglength >= 1:
-    if arglength == 1:
-        print(f"{arglength} argument:")
-        print(f"{arglength}: {sys.argv[1]}")
+    num = len(argv)
+    if num - 1 == 0:
+        print("0 arguments.")
+    elif num - 1 == 1:
+        print("{:d} argument:".format(num - 1))
     else:
-        print(f"{arglength} arguments:")
-        for i in range(1, len(sys.argv)):
-            print(f"{i}: {sys.argv[i]}")
+        print("{:d} arguments:".format(num - 1))
+    for i in range(1, num):
+        print("{:d}: {}".format(i, argv[i]))
