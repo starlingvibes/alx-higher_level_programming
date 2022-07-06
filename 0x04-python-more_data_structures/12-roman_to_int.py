4495e3a80c441996aa4cba9cbb265cb8a9e63949
starlingvibes <starlingroot@protonmail.com>
2022-07-06 10:30:37 -0700

    handled edge case; added whitespace in dict

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -3,7 +3,7 @@ def roman_to_int(roman_string):
     if type(roman_string) is not str or len(roman_string) == 0:
         return 0
     roman_arabic = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100,
-                    'D': 500, 'M':1000}
+                    'D': 500, 'M': 1000}
     result = 0
     for i in range(len(roman_string) - 1):
         first = roman_arabic[roman_string[i]]
