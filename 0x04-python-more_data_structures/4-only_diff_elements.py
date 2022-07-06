526eee227990e9edf4465a93e252480f38387f6a
starlingvibes <starlingroot@protonmail.com>
2022-07-06 07:30:43 -0700

    only differents

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -1,3 +1,7 @@
 #!/usr/bin/python3
 def only_diff_elements(set_1, set_2):
-    return set_1.difference(set_2)
+    if not set_1:
+        set_1 = set()
+    if not set_2:
+        set_2 = set()
+    return set_1.symmetric_difference(set_2)
