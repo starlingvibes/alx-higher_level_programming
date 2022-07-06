4fa1a5a527d6a0ab6f3fdf0b0a1ec5a0c8c3a303
starlingvibes <starlingroot@protonmail.com>
2022-07-06 10:09:11 -0700

    convert roman to int

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    roman_arabic = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100,
+                    'D': 500, 'M':1000}
+    result = 0
+    for i in range(len(roman_string) - 1):
+        first = roman_arabic[roman_string[i]]
+        second = roman_arabic[roman_string[i + 1]]
+        if first < second:
+            result += -first
+        else:
+            result += first
+    result += roman_arabic[roman_string[len(roman_string) - 1]]
+    return result
