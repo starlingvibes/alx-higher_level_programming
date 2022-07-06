acd14046a359b1aba8a56f347f70dd7584465ef1
starlingvibes <starlingroot@protonmail.com>
2022-07-06 08:44:24 -0700

    best score

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary is not None:
-        max_score = max(a_dictionary.values())
-        for name, score in a_dictionary.items():
-            if score == max_score:
-                return [name][0]
-    else:
+    if not a_dictionary:
         return None
+    max_score = max(a_dictionary.values())
+    for name, score in a_dictionary.items():
+        if score == max_score:
+            return [name][0]
