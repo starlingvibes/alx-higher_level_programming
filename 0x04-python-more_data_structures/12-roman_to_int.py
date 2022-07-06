384b408ec69672c02c5c9d56a6b3c9a8679a0014
starlingvibes <starlingroot@protonmail.com>
2022-07-06 10:28:14 -0700

    handled edge case

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
+    if type(roman_string) is not str or len(roman_string) == 0:
+        return 0
     roman_arabic = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100,
                     'D': 500, 'M':1000}
     result = 0
@@ -7,7 +9,7 @@ def roman_to_int(roman_string):
         first = roman_arabic[roman_string[i]]
         second = roman_arabic[roman_string[i + 1]]
         if first < second:
-            result += -first
+            result -= first
         else:
             result += first
     result += roman_arabic[roman_string[len(roman_string) - 1]]
