dec98abcbc48bb0d97c646faa6d7af199731ccf7
starlingvibes <starlingroot@protonmail.com>
2022-07-06 06:42:41 -0700

    search and replace

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    for i in range(len(my_list)):
-        if my_list[i] == search:
-            my_list[i] = replace
-    return my_list
+    new_list = list(map(lambda x: replace if x == search else x, my_list))
+    return new_list
