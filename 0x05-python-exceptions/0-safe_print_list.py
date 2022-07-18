707becc04d960ed79c8c2ebedb747f00d8454d15
starlingvibes <starlingroot@protonmail.com>
2022-07-18 07:05:36 -0700

    avoided using bare except clause

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -5,7 +5,7 @@ def safe_print_list(my_list=[], x=0):
         try:
             print("{}".format(my_list[i]), end="")
             counter += 1
-        except:
+        except IndexError:
             pass
     print("")
     return counter
