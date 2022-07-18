50d5863be3868409358d51fd415c1e7ca315d324
starlingvibes <starlingroot@protonmail.com>
2022-07-18 07:35:17 -0700

    updated exception

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -2,7 +2,7 @@
 def safe_print_integer(value):
     try:
         print("{:d}".format(value))
-    except ValueError:
+    except BaseException:
         return False
     else:
         return True
