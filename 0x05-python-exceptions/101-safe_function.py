71b5af779f5009f60859a9a7565fe570c711b94f
starlingvibes <starlingroot@protonmail.com>
2022-07-19 03:59:14 -0700

    executing a function safely

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+from sys import stderr
+
+def safe_function(fct, *args):
+    try:
+        result = fct(*args)
+    except BaseException as e:
+        print("Exception: {}".format(e), file=stderr)
+        result = None
+    finally:
+        return result
