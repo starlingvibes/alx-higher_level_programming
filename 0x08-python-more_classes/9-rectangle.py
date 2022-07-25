ad443bcbd006f1009dc7ffd0cbdfb791bf8f1ee3
starlingvibes <starlingroot@protonmail.com>
2022-07-25 09:40:23 -0700

    made pep8 to stop crying

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -55,7 +55,7 @@ class Rectangle:
         if not isinstance(rect_2, Rectangle):
             raise TypeError("rect_2 must be an instance of Rectangle")
         return rect_1 if rect_1.area() >= rect_2.area() else rect_2
-    
+
     @classmethod
     def square(cls, size=0):
         return (cls(size, size))
