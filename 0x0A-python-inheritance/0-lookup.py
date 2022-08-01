50a164de9e79cc31d1683e2196a24ba6e9128777
starlingvibes <starlingroot@protonmail.com>
2022-08-01 14:14:19 -0700

    added function documentation

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,3 +1,11 @@
 #!/usr/bin/python3
+"""
+Finding a list of available attributes and methods of an object
+"""
+
+
 def lookup(obj):
+    """
+    Returns the list of attributes and methods for an object
+    """
     return dir(obj)
