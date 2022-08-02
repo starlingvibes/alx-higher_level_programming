cb4a099988fbd1b684fa9e5c09e5aea609ebdcba
starlingvibes <starlingroot@protonmail.com>
2022-08-02 07:00:20 -0700

    append to a file

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Append to a file
+"""
+
+
+def append_write(filename="", text=""):
+    """
+    Appends a string at the end of a text file and returns
+    the number of characters added
+    """
+    with open(filename, mode="w+") as file:
+        return file.write(text)
