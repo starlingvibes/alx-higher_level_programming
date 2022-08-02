3f209c1bfb0e23bf85d1b7a6c2e9ca5784f1f4d1
starlingvibes <starlingroot@protonmail.com>
2022-08-02 06:23:17 -0700

    opening files

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Read file
+"""
+
+
+def read_file(filename=""):
+    """
+    Reads a text file and prints it to stdout
+    """
+    with open(filename) as file:
+        print(file.read())
---
+++ 0x0B-python-input_output/README.md
@@ -0,0 +1,1 @@
+Reading and writing python files
