3c0b909c0c94bc30ded5da8d15e6502d0f04f287
starlingvibes <starlingroot@protonmail.com>
2022-08-02 01:58:50 -0700

    improve geometry class

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Geometry module
+"""
+
+
+class BaseGeometry:
+    """
+    A BaseGeometry class
+    """
+    def area(self):
+        raise Exception("area() is not implemented")
