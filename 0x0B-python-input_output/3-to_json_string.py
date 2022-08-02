fbcc37b54a403e9815bee15aed7953fde991467d
starlingvibes <starlingroot@protonmail.com>
2022-08-02 07:15:03 -0700

    to json string

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Convert to JSON string
+"""
+import json
+
+
+def to_json_string(my_obj):
+    """
+    Returns the JSON representation of an object
+    """
+    return json.dumps(my_obj)
