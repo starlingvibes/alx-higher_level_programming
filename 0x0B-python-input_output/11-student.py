6316984f082ad139d62a3278fdc9a5236c05fd64
starlingvibes <starlingroot@protonmail.com>
2022-08-03 16:42:46 -0700

    student class json

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Module 11-student.
+"""Module 13-student.
 Creates a Student class.
 """
 
@@ -11,6 +11,7 @@ class Student:
         - last_name
         - age
     Public method to_json().
+    Public method reload_from_json().
     """
 
     def __init__(self, first_name, last_name, age):
@@ -20,11 +21,30 @@ class Student:
         self.last_name = last_name
         self.age = age
 
-    def to_json(self):
+    def to_json(self, attrs=None):
         """Retrieves a dictionary representation
         of a Student instance.
+
+        Args:
+            - attrs: list of attributes
 
         Returns: the dict representation of the instance.
         """
 
+        my_dict = dict()
+        if attrs and all(isinstance(x, str) for x in attrs):
+            for x in attrs:
+                if x in self.__dict__:
+                    my_dict.update({x: self.__dict__[x]})
+            return my_dict
         return self.__dict__
+
+    def reload_from_json(self, json):
+        """Replaces all attributes of the Student instance.
+
+        Args:
+            - json: dictionnary of attributes
+        """
+
+        for x in json:
+            self.__dict__.update({x: json[x]})
