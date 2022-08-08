ee5abfd7cc14e4d29bbd2826db83c7c7dc9fb13b
starlingvibes <starlingroot@protonmail.com>
2022-08-08 16:05:12 -0700

    fixed buggy typo

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -21,5 +21,5 @@ class Base:
         if id is not None:
             self.id = id
         else:
-            Base.__nd_objects += 1
+            Base.__nb_objects += 1
             self.id = Base.__nb_objects
