eb4ae3a5bd1cafb25822c7480f0da77d45419bf1
starlingvibes <starlingroot@protonmail.com>
2022-09-08 08:32:24 -0700

    setting default column values

--- 0x0E-SQL_more_queries/3-force_name.sql
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -1,2 +1,2 @@
 -- creates force_name table in database
-CREATE TABLE IF NOT EXISTS force_name (id INT, name VARCHAR(256) IS NOT NULL);
+CREATE TABLE IF NOT EXISTS force_name (id INT, name VARCHAR(256) NOT NULL);
---
+++ 0x0E-SQL_more_queries/4-never_empty.sql
@@ -0,0 +1,2 @@
+-- create table with id having default value
+CREATE TABLE IF NOT EXISTS id_not_null (id INT DEFAULT 1, name VARCHAR(256));
