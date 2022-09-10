1f598d8ff4ccc63e2ab510ab31ec320060d4ad80
starlingvibes <starlingroot@protonmail.com>
2022-09-10 04:09:54 -0700

    create table with a unique default value

--- 0x0E-SQL_more_queries/5-unique_id.sql
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -1,2 +1,2 @@
 -- creates a table with a unique default value
-CREATE TABLE IF NOT EXISTS unique_id (id=1 INT UNIQUE, name VARCHAR(256));
+CREATE TABLE IF NOT EXISTS unique_id (id INT DEFAULT 1 UNIQUE, name VARCHAR(256));
