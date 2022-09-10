d18912b9c28fe527c2187694fa1ee5a83a5e715c
starlingvibes <starlingroot@protonmail.com>
2022-09-10 05:25:46 -0700

    removed aliasing

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,2 +1,2 @@
 -- lists all shows in hbtn_0d_tvshows with at least one genre linked
-SELECT tv.title, show.genre_id FROM tv_shows AS tv INNER JOIN tv_show_genres AS show ON tv.id = show.show_id ORDER BY tv.title ASC, show.genre_id ASC;
+SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows INNER JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
