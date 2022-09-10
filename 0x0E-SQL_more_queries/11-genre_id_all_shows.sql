b67df046b3dec0f4602ddd0dfbdfec1d4ebce5df
starlingvibes <starlingroot@protonmail.com>
2022-09-10 05:34:19 -0700

    genre id for all shows

---
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -0,0 +1,2 @@
+-- lists all shows container in the database hbtn_0d_tvshows
+SELECT tv_shows.title, tv_shows_genres.genre_id FROM tv_shows LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
