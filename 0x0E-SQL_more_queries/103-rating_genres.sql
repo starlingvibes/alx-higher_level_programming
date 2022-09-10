54c0b790d564dc172b58fddc2815c2b26fd04278
starlingvibes <starlingroot@protonmail.com>
2022-09-10 06:05:56 -0700

    list genres ordered by their rating

---
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -0,0 +1,7 @@
+-- lists all genres in the database by their rating
+SELECT name, SUM(tv_show_ratings.rate) 'rating'
+FROM tv_genres
+INNER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+INNER JOIN tv_show_ratings ON tv_show_genres.show_id = tv_show_ratings.show_id
+GROUP BY name
+ORDER BY rating DESC;
