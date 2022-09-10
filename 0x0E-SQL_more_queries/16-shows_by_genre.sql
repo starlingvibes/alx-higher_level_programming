287ae0d6b1b200ec6d8bd9351932584df47ae2b8
starlingvibes <starlingroot@protonmail.com>
2022-09-10 05:53:27 -0700

    list all shows, and genres linked to that show

---
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -0,0 +1,6 @@
+-- list all shows, and all genres linked to that show
+SELECT title, name
+FROM tv_shows
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
+ORDER BY title ASC, name ASC;
