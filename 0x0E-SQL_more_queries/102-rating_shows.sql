940e928e0d5a2a4b6da481c0ad64df4d781f53df
starlingvibes <starlingroot@protonmail.com>
2022-09-10 06:03:26 -0700

    rank shows by their rating

---
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -0,0 +1,6 @@
+-- lists all shows by their rating
+SELECT title, SUM(tv_show_ratings.rate) 'rating'
+FROM tv_shows
+LEFT JOIN tv_show_ratings ON tv_show_ratings.show_id = tv_shows.id
+GROUP BY title
+ORDER BY rating DESC;
