fd43343c3b7e0c5895dbcd7115cd4f1069fb20ca
starlingvibes <starlingroot@protonmail.com>
2022-10-10 07:34:46 -0700

    fixed some over indentation

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -20,5 +20,5 @@ if __name__ == "__main__":
     for state, city in session.query(State, City)\
                               .filter(City.state_id == State.id)\
                               .order_by(City.id).all():
-            print("{}: ({}) {}".format(state.name, city.id, city.name))
+        print("{}: ({}) {}".format(state.name, city.id, city.name))
     session.close()
