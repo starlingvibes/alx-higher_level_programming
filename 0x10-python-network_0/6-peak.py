0669cf70e8aaada366a35a0e7307fa8e6df22d5d
starlingvibes <starlingroot@protonmail.com>
2022-10-23 13:25:25 -0700

    made pycodestyle happy

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Finds a peak in a list of unsorted integers"""
 
+
 def find_peak(list_of_integers):
     """Finds a peak in list_of_integers"""
     if list_of_integers is None or len(list_of_integers) == 0:
